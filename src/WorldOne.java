5a1257bf8b2ea7a01716fa632fd861a15d2b0137
vaibhavi <vaibhavi.pawar28sept@gmail.com>
2023-01-12 12:46:22 +0530

    This is second and third world program

--- src/WorldOne.java
+++ src/WorldOne.java
@@ -4,6 +4,8 @@ public class WorldOne {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is world one program");
+		System.out.println("This is world second program");
+		System.out.println("This is world third program");
 	}
 
 }
